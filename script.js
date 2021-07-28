2f526d3b708d6914ecc963564fbd6980ecc960a0
redetalaw <87781550+redetalaw@users.noreply.github.com>
2021-07-28 15:25:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,81 +3,112 @@
 const topBar = document.querySelector('.topbar');
 const errBar = document.querySelector('.errbar');
 
-const arrDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const arrDay = ['domingo','segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta-feira','sábado'];
+const arrCond = {
+	clearday: "limpo",
+	clearnight: "limpo",
+	pcloudyday: "nuvens esparsas",
+	pcloudynight: "nuvens esparsas",
+	mcloudyday: "parcialmente nublado",
+	mcloudynight: "parcialmente nublado",
+	cloudyday: "nublado",
+	cloudynight: "nublado",
+	humidday: "parcialmente nublado ",
+	humidnight: "parcilamente nublado",
+	lightrainday: "chuvas leves",
+	lightrainnight: "chuvas leves",
+	oshowerday: "chuvas ocasionais",
+	oshowernight: "chuvas ocasionais",
+	ishowerday: "chuvas ocasionais",
+	ishowernight: "chuvas ocasionais",
+	lightsnowday: "neve leve", 
+	lightsnownight: "neve leve",
+	rainday: "chuva",
+	rainnight: "chuva",
+	snowday: "neve",
+	snownight: "neve",
+	rainsnowday: "granizo",
+	rainsnownight: "granizo",
+	tsday: "raios",
+	tsnight: "raios",
+	tsrainday: "chuva e raios",
+	tsrainnight: "chuva e raios"
+}
 
-const getPosition = function () {
-	//returns a promise using the geolocation API that is not a promise
-	//but still is an asynchronous function
-	return new Promise(function (resolve, reject) {
-    // navigator.geolocation.getCurrentPosition(
-    //   position => resolve(position),
-    //   err => reject(err)
-    // );
+const getNavPos = function() {
+	return new Promise(function(resolve, reject) {
 		navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
+	});
+}
 
-const test = async function() {
+//const lat = -27.954183;
+//const lng = -49.877898;
+
+const getWeatherJSON = async function(navPos) {
 	try {
-		const pos = await getPosition();
-		//if(!pos.ok) throw new Error('Navigator.geolocation error');
-		//console.log(pos)
-		
-		//decosntruct to rename variables
-		const {latitude: lat, longitude: lng} = pos.coords;
-		//const lat = -27.955804;
-		//const lng = -49.876213;
-		
-		//fetching data from reverse geolocation API 
-		const revGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
-		if(!revGeo.ok) throw new Error('Reverse geolocation error');
-		
-		const revGeoJSON = await revGeo.json();
-		console.log(revGeoJSON);
-		const city = revGeoJSON.city ? revGeoJSON.city : revGeoJSON.locality+ "*";
-		const state = revGeoJSON.principalSubdivision;
-		const country = revGeoJSON.countryName;
-		
-		//fetching data from 7timer API for weather report
-		const res = await fetch(`https://www.7timer.info/bin/api.pl?lon=${lng}&lat=${lat}&product=civillight&output=json`);
-		if(!res.ok) throw new Error('7timer error');
-		
-		const weatherJSON = await res.json();
-		const weather = weatherJSON.dataseries;
-		console.log(weather);
-		
-		let html = `<p>7timer: 7 day weather forecast<br>
-					BigDataCloud: Reverse Geolocation</p>
-					<hr>
-					<p>Current Location: ${city} - ${state} - ${country}<br>
-					<font size = "-2">* closest approximation, errors may occur</font></p>`;
-
-		weather.forEach(function(item){
-			html += `
-				<hr>
-				<p>Date: ${formatDate(item.date)}<br>
-				Weather: ${item.weather}<br>
-				Min: ${formatNumber(item.temp2m.min)}°C &nbsp &nbsp &nbsp Max: ${formatNumber(item.temp2m.max)}°C</p>`;
-		});
-		
-		topBar.insertAdjacentHTML('beforeend', html);
-		
-	} catch(err) {
+		const {latitude: lat, longitude: lng} = navPos.coords;
+		const owmWeather = await fetch(`https://www.7timer.info/bin/api.pl?lon=${lng}&lat=${lat}&product=civil&output=json`);
+		const owmWeatherJSON = await owmWeather.json();
+		return owmWeatherJSON;
+	} 
+	catch (err) {
 		console.log(err.message);
-		errBar.insertAdjacentHTML('beforeend', err.message);
 	}
 }
 
-const formatDate = function(date) {
-	const dateStr = String(date);
+const getGeoJSON = async function(navPos) {
+	try {
+		const {latitude: lat, longitude: lng} = navPos.coords;
+		const owmRev = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=pt`);
+		const owmRevJSON = await owmRev.json();
+		return owmRevJSON;
+	}
+	catch (err) {
+		console.log(err.message);
+	}
+}
+
+const updateHtml = function(revGeoJSON, owmWeatherJSON) {
+	const city = revGeoJSON.city ? revGeoJSON.city : revGeoJSON.locality + "*";
+	const state = revGeoJSON.principalSubdivision;
+	const country = revGeoJSON.countryName;
+	const weather = owmWeatherJSON.dataseries;
+	const initialTime = formatTime(owmWeatherJSON.init);
 	
-	const year = dateStr.slice(0,4);
-	const month = dateStr.slice(4,6);
-	const day = dateStr.slice(6,8);
+	let html = `<p>7timer: API de Previsão de Tempo<br>
+				BigDataCloud: API de Geolocalização Reversa</p>
+				<hr>
+				<p>Localização*: ${city} - ${state} - ${country}<br>
+				<font size = "-2">* Aproximação, pode conter erros</font>
+				<hr>
+				</p>`;
+
+	weather.forEach(function(item){
+		html += `
+			<hr>
+			<p>Data: ${formatDate(formatTime(owmWeatherJSON.init, item.timepoint-3))}<br>
+			Tempo: ${arrCond[item.weather]}<br>
+			Temperatura: ${formatNumber(item.temp2m)}°C &nbsp &nbsp Umidade: ${item.rh2m}</p>`;
+	});
 	
-	const dayWeek = new Date(`${month}/${day}/${year}`);
+	topBar.insertAdjacentHTML('beforeend', html);
+}
+
+const formatDate = function(data) {
 	
-	return `${day}/${month}/${year}, ${arrDay[dayWeek.getDay()]}`;
+	return `${arrDay[data.getDay()]}, ${formatNumber(data.getDate())}/${formatNumber(data.getMonth()+1)}/${data.getFullYear()}, ${formatNumber(data.getHours())}:00`;
+}
+
+const formatTime = function(date, offset = 0) {
+	
+	const year = date.slice(0,4);
+	const month = date.slice(4,6);
+	const day = date.slice(6,8);
+	const hour = date.slice(-2);
+	
+	const d = new Date(Number(year), Number(month)-1, Number(day), Number(hour) + offset, 0, 0);
+	
+	return d;
 }
 
 const formatNumber = function(n) {
@@ -87,4 +118,102 @@ const formatNumber = function(n) {
 		return n;
 }
 
-test();
+const main = async function() {
+	try {
+		const navPos = await getNavPos();
+		const geo = await getGeoJSON(navPos);
+		const weath = await getWeatherJSON(navPos);
+		console.log(navPos, geo, weath);
+		
+		updateHtml(geo, weath);
+	}
+	catch (err) {
+		console.log(err.message)
+	}
+}
+
+main();
+
+// const getPosition = function () {
+	// //returns a promise using the geolocation API that is not a promise
+	// //but still is an asynchronous function
+	// return new Promise(function (resolve, reject) {
+    // // navigator.geolocation.getCurrentPosition(
+    // //   position => resolve(position),
+    // //   err => reject(err)
+    // // );
+		// navigator.geolocation.getCurrentPosition(resolve, reject);
+  // });
+// };
+
+// const main = async function() {
+	// try {
+		// const pos = await getPosition();
+		// //if(!pos.ok) throw new Error('Navigator.geolocation error');
+		// //console.log(pos)
+		
+		// //decosntruct to rename variables
+		// const {latitude: lat, longitude: lng} = pos.coords;
+		// //const lat = -15.644514;
+		// //const lng = -47.848138;
+		
+		// //fetching data from reverse geolocation API 
+		// const revGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=pt`);
+		// if(!revGeo.ok) throw new Error('Erro ao usar geolocalização reversa');
+		
+		// const revGeoJSON = await revGeo.json();
+		// console.log(revGeoJSON);
+		// const city = revGeoJSON.city ? revGeoJSON.city : revGeoJSON.locality + "*";
+		// const state = revGeoJSON.principalSubdivision;
+		// const country = revGeoJSON.countryName;
+		
+		// //fetching data from 7timer API for weather report
+		// const res = await fetch(`https://www.7timer.info/bin/api.pl?lon=${lng}&lat=${lat}&product=civillight&output=json`);
+		// if(!res.ok) throw new Error('Erro na previsão');
+		
+		// const weatherJSON = await res.json();
+		// const weather = weatherJSON.dataseries;
+		// console.log(weather);
+		
+		// let html = `<p>7timer: API de previsão de tempo<br>
+					// BigDataCloud: API de geolocalização reversa</p>
+					// <hr>
+					// <p>Localização*: ${city} - ${state} - ${country}<br>
+					// <font size = "-2">* closest approximation, errors may occur</font></p>`;
+
+		// weather.forEach(function(item){
+			// html += `
+				// <hr>
+				// <p>Data: ${formatDate(item.date)}<br>
+				// Tempo: ${item.weather}<br>
+				// Mín: ${formatNumber(item.temp2m.min)}°C &nbsp &nbsp &nbsp Máx: ${formatNumber(item.temp2m.max)}°C</p>`;
+		// });
+		
+		// topBar.insertAdjacentHTML('beforeend', html);
+		
+	// } catch(err) {
+		// console.log(err.message);
+		// errBar.insertAdjacentHTML('beforeend', err.message);
+	// }
+// }
+
+// const formatDate = function(date) {
+	// const dateStr = String(date);
+	
+	// const year = dateStr.slice(0,4);
+	// const month = dateStr.slice(4,6);
+	// const day = dateStr.slice(6,8);
+	
+	// const dayWeek = new Date(`${month}/${day}/${year}`);
+	
+	// return `${day}/${month}/${year}, ${arrDay[dayWeek.getDay()]}`;
+// }
+
+// const formatNumber = function(n) {
+	// if (n < 10 && n > -1)
+		// return (('0'+n).slice(-2))
+	// else
+		// return n;
+// }
+
+// main();
